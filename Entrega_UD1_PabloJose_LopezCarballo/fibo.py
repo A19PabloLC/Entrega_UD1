12ad45adbfceaa9cd01f06ecccf29d6644bc63d6
A19PabloLC <150478203+A19PabloLC@users.noreply.github.com>
2023-11-26 19:16:53 +0100

    Add files via upload

--- Entrega_UD1_PabloJose_LopezCarballo/fibo.py
+++ Entrega_UD1_PabloJose_LopezCarballo/fibo.py
@@ -1,6 +1,6 @@
 ## La escala de Fibonacci es: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, etc
 
-def fibona(n):
+def fibonacci(n):
 #Se creara una funcion para definir la escala de Fibonacci
 
     if n < 0:
@@ -13,8 +13,8 @@ def fibona(n):
         return 1
 #La posicion 2 y 3 son iguales, son 1
     else:
-        return fibona(n - 1) + fibona(n - 2)
+        return fibonacci(n - 1) + fibonacci(n - 2)
 #Utilizando la formula matematica, se podra averiguar el numero de cada posicion en la secuencia a partir de la posicion 4
 
-print(fibona(5))
+print(fibonacci(5))
 #Poniendo un numero 5 que se corresponde con la posicion 5 de Fibonacci, nos indicara el numero 3
--- Entrega_UD1_PabloJose_LopezCarballo/test_fibo.py
+++ Entrega_UD1_PabloJose_LopezCarballo/test_fibo.py
@@ -1,12 +1,15 @@
 import unittest
 #Se importa la libreria unittest
 
-from fibo import fibona
+from fibo import fibonacci
 #Se importa del archivo fibo.py la funcion fibona
 
 class TestMain(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(fibona(5), 3)
+        self.assertEqual(fibonacci(5),3)
 #Se pone en fibona la posicion 5 y se indica que su resultado debe ser igual a 3
+
+if __name__== '__main__':
+    unittest.main()
 
 #Una vez hecho
