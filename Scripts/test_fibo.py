abe1fc9f5a2ad684557d0f9cf237e72e1024ef69
A19PabloLC <150478203+A19PabloLC@users.noreply.github.com>
2023-11-27 19:17:52 +0100

    Add files via upload

---
+++ Scripts/test_fibo.py
@@ -0,0 +1,15 @@
+import unittest
+#Se importa la libreria unittest
+
+from fibo import fibonacci
+#Se importa del archivo fibo.py la funcion fibonacci
+
+class TestMain(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(fibonacci(5),3)
+#Se pone en fibonacci la posicion 5 y se indica que su resultado debe ser igual a 3
+
+if __name__== '__main__':
+    unittest.main()
+
+#Es necesario name = main puesto que si no, no ejecutara el test para compararlos
