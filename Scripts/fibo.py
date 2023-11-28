bbc32f8ddf916d1f25995e84ebab26af3f0b3682
A19PabloLC <150478203+A19PabloLC@users.noreply.github.com>
2023-11-28 17:04:27 +0100

    Add files via upload

--- Scripts/fibo.py
+++ Scripts/fibo.py
@@ -2,19 +2,18 @@
 
 def fibonacci(n):
 #Se creara una funcion para definir la escala de Fibonacci
-
     if n < 0:
         print("Valor incorrecto")
 #No se admitiran valores menores que 0
     elif n == 0 or n == 1:
         return 0
-#La posicion 0 y 1 son iguales, son 0
+#Si se pone la posicion 0 o 1, devuelve 0
     elif n == 2 or n == 3:
         return 1
-#La posicion 2 y 3 son iguales, son 1
+#Si se pone la posicion 2 o 3, devuelve 1
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
-#Utilizando la formula matematica, se podra averiguar el numero de cada posicion en la secuencia a partir de la posicion 4
+#Utilizando la formula matematica de la escala de Fibonacci, se podra averiguar el numero de cada posicion en la secuencia a partir de la posicion 4
 
 print(fibonacci(5))
 #Poniendo un numero 5 que se corresponde con la posicion 5 de Fibonacci, nos indicara el numero 3
--- Scripts/test_fibo.py
+++ Scripts/test_fibo.py
@@ -1,13 +1,15 @@
-import unittest
-#Se importa la libreria unittest
-
 from fibo import fibonacci
 #Se importa del archivo fibo.py la funcion fibonacci
 
-class TestMain(unittest.TestCase):
-    def comprobar_fibo(self):
-        self.assertEqual(fibonacci(5),3)
-#Se pone en fibonacci la posicion 5 y se indica que su resultado debe ser igual a 3
+import unittest
+#Se importa la libreria unittest
+
+class CompruebaFibonacci(unittest.TestCase):
+#Se crea una clase nueva y se utiliza TestCase para comprobar si se cumple el resultado esperado de la funcion de Fibonacci
+    def test_fibonacci(self):
+#Se crea una funcion nueva y debe ponerse obligatoriamente al principio 'test' en el nombre de la funcion, porque si no, no realizara ningun test
+        self.assertEqual(fibonacci(5), 3)
+#Se pone en la funcion fibonacci la posicion 5 y se indica que su resultado debe ser igual a 3
 
 if __name__== '__main__':
     unittest.main()
