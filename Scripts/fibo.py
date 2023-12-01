6030967bb685fee5fade91a2d9637b7c35735e1a
A19PabloLC <150478203+A19PabloLC@users.noreply.github.com>
2023-12-01 18:12:34 +0100

    Update fibo.py

--- Scripts/fibo.py
+++ Scripts/fibo.py
@@ -1,7 +1,7 @@
 #La sucesion de Fibonacci es: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, etc
 
 def fibonacci(n):
-#Se creara una funcion para definir la escala de Fibonacci
+#Se creara una funcion para definir la sucesion de Fibonacci
     if n < 0:
         print("Valor incorrecto")
 #No se admitiran valores menores que 0
@@ -13,7 +13,7 @@ def fibonacci(n):
 #Si se pone la posicion 2 o 3, devuelve 1
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
-#Utilizando la formula matematica de la escala de Fibonacci, se podra averiguar el numero de cada posicion en la secuencia a partir de la posicion 4
+#Utilizando la formula matematica de la sucesion de Fibonacci, se podra averiguar el numero de cada posicion en la sucesion a partir de la posicion 4
 
 print(fibonacci(5))
 #Poniendo un numero 5 que se corresponde con la posicion 5 de Fibonacci, nos devolvera el numero 3
